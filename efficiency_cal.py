7b36c5546fe76fd78051e7b94795ab7568d622de
Peilin-Yang <yangpeilyn@gmail.com>
2017-09-01 22:59:54 -0400

    fix efficiency calculation

--- efficiency_cal.py
+++ efficiency_cal.py
@@ -16,13 +16,17 @@ def collect_all(log_root):
     return all_log_files
 
 def get_duration(fn, _type='index'):
-    with open(fn) as f:
-        last_line = f.readlines()[-1]
-    t = last_line.split(':')
-    if _type == 'index':
-        return int(t[0])*60 + int(t[1])
-    elif _type == 'ranking':
-        return int(t[0])*3600 + int(t[1])*60 + int(t[2])
+    try:
+        with open(fn) as f:
+            last_line = f.readlines()[-1]
+        t = last_line.split(':')
+        if _type == 'index':
+            return int(t[0])*60 + int(t[1])
+        elif _type == 'ranking':
+            return int(t[0])*3600 + int(t[1])*60 + int(t[2])
+    except:
+        print('Something wrong with file:%s' % fn)
+        return -1
  
 def mean(data):
     """Return the sample arithmetic mean of data."""
@@ -41,7 +45,7 @@ def pstdev(data):
     """Calculates the population standard deviation."""
     n = len(data)
     if n < 2:
-        raise ValueError('variance requires at least two data points')
+        return 0
     ss = _ss(data)
     pvar = ss/n # the population variance
     return pvar**0.5
@@ -53,19 +57,19 @@ def cal_efficiency(args):
         all_stats[k] = []
         for fn in all_logs[k]:
             duration = get_duration(fn, args.type)
-            all_stats[k].append(duration)
-    final_stats = {}
+            if duration > 0:
+                all_stats[k].append(duration)
     if args.type == 'index':
         for collection in sorted(all_stats):
             print(collection, '%s'%str(datetime.timedelta(seconds=round(mean(all_stats[collection]), 0))), '%.1f'%pstdev(all_stats[collection]))
     if args.type == 'ranking':
+        final_stats = {}
         for k in all_stats:
             collection, model = k.split('.')
             if collection not in final_stats:
                 final_stats[collection] = {}
             if model not in final_stats[collection]:
                 final_stats[collection][model] = all_stats[k]
-        print(final_stats)
         for collection in sorted(final_stats):
             for model in sorted(final_stats[collection]):
                     l = final_stats[collection][model]
